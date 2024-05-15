ab2a98525f725c6d5bcbf3eb4c84a3b232a0044a
Kilerwin <miguel642013@gmail.com>
2024-05-15 16:52:03 -0500

    feat error cuenta desabilidata y  accioon no permitida Se crea un primer error para indicar que la cuenta ya se encuentra desabilitada Se crea un segundo error para indicar que la accion no esta permitida

--- schemas/errorSchema.js
+++ schemas/errorSchema.js
@@ -10,3 +10,5 @@ function ErrorFactory (name) {
 export const NoData = ErrorFactory('There is no data')
 export const DuplicateInfo = ErrorFactory('Existing information')
 export const DocumentInUse = ErrorFactory('Document is in use')
+export const AccountAlreadyDisable = ErrorFactory('User is disable')
+export const ActionNotAllowed = ErrorFactory('Action no allowed')
