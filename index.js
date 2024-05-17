ed43c2c3b312ead57679332389347bfca9e2fc9a
jose-dev1 <jose.cm25@hotmail.com>
2024-05-17 12:19:12 -0500

    validacion jwt v1.0

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express, { json } from 'express'
 import { PORT } from './config/envVariables.js'
+import LoginRouter from './routes/loginRouter.js'
 import corsMiddleware from './middlewares/cors.middleware.js'
 
 const app = express()
 app.use(corsMiddleware())
 app.use(json())
 
-app.get('/', (req, res) => res.json('Hello World!'))
+app.use('/login', LoginRouter)
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT ?? 1234}`))
---
+++ models/loginModel.js
@@ -0,0 +1,9 @@
+import db from '../config/database.js';
+
+export default class LoginModel {
+    static async getUserByUsername(correo_usuario) {
+        console.log(correo_usuario);
+        const [user] = await db.query('SELECT BIN_TO_UUID(id_usuario) AS id, correo_usuario, password_usuario, numero_documento_usuario, primer_nombre_usuario, primer_apellido_usuario, telefono_usuario, direccion_usuario, id_genero, id_tipo_usuario FROM usuarios WHERE correo_usuario = ?', [correo_usuario]);
+        return user;
+    }
+}
