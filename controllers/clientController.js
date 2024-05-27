a719a67010a0f7f680e8193318141af18b5327f7
Kilerwin <miguel642013@gmail.com>
2024-05-27 11:29:55 -0500

    fix contoller client Se arregla error por el cual se enviaban dos respuestas al momento de registrar un cliente cuando se generaba un error

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -15,8 +15,8 @@ export class ClientController {
     const result = validateClientDataCreate(req.body)
     if (!result.success) return res.status(400).json(`${JSON.parse(result.error.message)[0].message}`)
     const response = await ClientModel.createClient(result.data)
-    if (response instanceof DuplicateInfo) res.status(400).json({ message: 'El cliente ya esta registrado' })
-    if (response instanceof Error) res.status(500).json({ message: 'Error interno del servidor ' })
+    if (response instanceof DuplicateInfo) return res.status(400).json({ message: 'El cliente ya esta registrado' })
+    if (response instanceof Error) return res.status(500).json({ message: 'Error interno del servidor ' })
     return res.json({ message: 'Cliente creado exitosamente' })
   }
 }
