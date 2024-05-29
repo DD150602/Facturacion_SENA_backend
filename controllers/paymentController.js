6e8211db14a69fcb2af1f462b4ce5a27f7385bbe
Kilerwin <miguel642013@gmail.com>
2024-05-28 20:51:47 -0500

    feat cargar cobros Se crea la logica para cargar los cobros que realizara el empleado en el dia de trabajo

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,7 +1,6 @@
 import { PaymentModel } from '../models/paymentModel.js'
 import { NoData } from '../schemas/errorSchema.js'
 import { validateUserById } from '../schemas/user.js'
-import { validateBillById, validateCreatePayment } from '../schemas/payment.js'
 
 export class PaymentController {
   static async getData (req, res) {
@@ -10,24 +9,6 @@ export class PaymentController {
     if (!result.success) return res.status(400).json(`${JSON.parse(result.error.message)[0].message}`)
     const response = await PaymentModel.getData(result.data)
     if (response instanceof NoData) return res.status(404).json('No se han encontrado datos para cargar.')
-    if (response instanceof Error) return res.status(500).json('Error interno en el servidor')
-    return res.json(response)
-  }
-
-  static async getDataById (req, res) {
-    const { id } = req.params
-    const result = validateBillById({ id })
-    if (!result.success) return res.status(400).json(`${JSON.parse(result.error.message)[0].message}`)
-    const response = await PaymentModel.getDataById(result.data)
-    if (response instanceof NoData) return res.status(404).json('No se han encontrado datos para cargar.')
-    if (response instanceof Error) return res.status(500).json('Error interno en el servidor')
-    return res.json(response)
-  }
-
-  static async createPayment (req, res) {
-    const result = validateCreatePayment(req.body)
-    if (!result.success) return res.status(400).json(`${JSON.parse(result.error.message)[0].message}`)
-    const response = await PaymentModel.createPayment(result.data)
     if (response instanceof Error) return res.status(500).json('Error interno en el servidor')
     return res.json(response)
   }
--- models/paymentModel.js
+++ models/paymentModel.js
@@ -1,48 +1,24 @@
-import db from '../config/database.js'
-import { NoData } from '../schemas/errorSchema.js'
-export class PaymentModel {
-  static async getData ({ id }) {
+import db from "../config/database.js"
+import { NoData } from "../schemas/errorSchema.js"
+export class PaymentModel{
+  static async getData ({id}){
     try {
       const [data] = await db.query(`SELECT BIN_TO_UUID(id_factura) id, cuota_actual_factura ,valor_neto_factura,cantidad_cuotas_factura, CONCAT_WS(' ',primer_nombre_cliente,primer_apellido_cliente), direccion_cliente, telefono_cliente
       FROM facturas
       INNER JOIN clientes on facturas.id_cliente = clientes.id_cliente
       INNER JOIN usuarios on facturas.id_usuario = usuarios.id_usuario
-      WHERE usuarios.id_usuario=UUID_TO_BIN(?) AND cuota_actual_factura<=cantidad_cuotas_factura AND fecha_proximo_pago<=CURDATE()`, [id])
-      if (data.length === 0) throw new NoData()
+      WHERE usuarios.id_usuario=UUID_TO_BIN(?) AND cuota_actual_factura<=cantidad_cuotas_factura AND fecha_proximo_pago<=CURDATE()`,[id])
+      if(data.length === 0) throw new NoData()
       return data
     } catch (error) {
       return error
     }
   }
-
-  static async getDataById ({ id }) {
+  static async createPayment(input){
     try {
-      const [[data]] = await db.query(`SELECT cuota_actual_factura ,valor_neto_factura,cantidad_cuotas_factura,facturas.id_tipo_cuota,descripcion_cuota
-      FROM facturas
-      INNER JOIN tipo_cuotas on facturas.id_tipo_cuota = tipo_cuotas.id_tipo_cuota
-      WHERE id_factura = ?`, [id])
-      if (!data) throw new NoData()
-      return data
+      const {entidadBancaria, idTipoTransaccion, numeroCuota, valorCuota, idFactura, fechaProximoPago} = input
     } catch (error) {
-      return error
-    }
-  }
-
-  static async createPayment (input) {
-    try {
-      const { entidadBancaria, idTipoTransaccion, numeroCuota, valorCuota, idFactura, fechaProximoPago } = input
-      await db.beginTransaction()
-      await db.query('INSERT INTO transacciones (entidad_bancaria,id_tipo_transaccion) VALUES (?,?)', [entidadBancaria, idTipoTransaccion])
-      const [[lastInsertId]] = await db.query(
-        'SELECT BIN_TO_UUID(id_transaccion) id_transaccion FROM transacciones ORDER BY fecha_transaccion DESC LIMIT 1'
-      )
-      await db.query('INSERT INTO cuotas_factura (numero_cuota,valor_cuota,id_transaccion,id_factura) vaues(?,?,UUID_TO_BIN(?),?)', [numeroCuota, valorCuota, lastInsertId.id_transaccion, idFactura])
-      await db.query('UPDATE facturas SET cuota_actual_factura = ? , fecha_proximo_pago = ? WHERE id_factura=?', [numeroCuota + 1, fechaProximoPago, idFactura])
-      await db.commit()
-      return 'Abono generado satisfactoriamente'
-    } catch (error) {
-      await db.rollback()
-      return error
+      
     }
   }
 }
