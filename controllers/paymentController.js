703138d868998b8a3d1778362a26fa4c01a3982b
Kilerwin <miguel642013@gmail.com>
2024-05-29 08:51:00 -0500

    Feat create payment Se crea la logica para cargar datos de un factura particular seleccionada Se crea logica para generar pagos de una factura ya creada anteriormente

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,14 +1,33 @@
-import { PaymentModel } from "../models/paymentModel.js"
-import { NoData } from "../schemas/errorSchema.js"
-import { validateUserById } from "../schemas/user.js"
+import { PaymentModel } from '../models/paymentModel.js'
+import { NoData } from '../schemas/errorSchema.js'
+import { validateUserById } from '../schemas/user.js'
+import { validateBillById, validateCreatePayment } from '../schemas/payment.js'
 
 export class PaymentController {
-  static async getData(req,res){
+  static async getData (req, res) {
     const { id } = req.params
     const result = validateUserById({ id })
     if (!result.success) return res.status(400).json(`${JSON.parse(result.error.message)[0].message}`)
     const response = await PaymentModel.getData(result.data)
     if (response instanceof NoData) return res.status(404).json('No se han encontrado datos para cargar.')
+    if (response instanceof Error) return res.status(500).json('Error interno en el servidor')
+    return res.json(response)
+  }
+
+  static async getDataById (req, res) {
+    const { id } = req.params
+    const result = validateBillById({ id })
+    if (!result.success) return res.status(400).json(`${JSON.parse(result.error.message)[0].message}`)
+    const response = await PaymentModel.getDataById(result.data)
+    if (response instanceof NoData) return res.status(404).json('No se han encontrado datos para cargar.')
+    if (response instanceof Error) return res.status(500).json('Error interno en el servidor')
+    return res.json(response)
+  }
+
+  static async createPayment (req, res) {
+    const result = validateCreatePayment(req.body)
+    if (!result.success) return res.status(400).json(`${JSON.parse(result.error.message)[0].message}`)
+    const response = await PaymentModel.createPayment(result.data)
     if (response instanceof Error) return res.status(500).json('Error interno en el servidor')
     return res.json(response)
   }
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,3 +3,5 @@ import { PaymentController } from '../controllers/paymentController.js'
 export const paymentRoutes = Router()
 
 paymentRoutes.get('/:id', PaymentController.getData)
+paymentRoutes.get('/abono/:id', PaymentController.getDataById)
+paymentRoutes.post('/', PaymentController.createPayment)
