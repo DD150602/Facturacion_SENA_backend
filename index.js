be310818f9d07e8f56c817ae915f93d065dcd271
Kilerwin <miguel642013@gmail.com>
2024-05-28 20:51:47 -0500

    feat cargar cobros Se crea la logica para cargar los cobros que realizara el empleado en el dia de trabajo

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import productRouter from './routes/productRoutes.js'
 import { ClientRoute } from './routes/clientRoutes.js'
 import { uploadRouter } from './routes/uploadFilesRoutes.js'
 import repoteVentasRouter from './routes/reporteVentasRouter.js'
+import { paymentRoutes } from './routes/paymentRoutes.js'
 import { FacturaRoute } from './routes/facturaRoutes.js'
 
 const app = express()
@@ -22,6 +23,7 @@ app.use('/login', LoginRouter)
 app.use('/cliente', ClientRoute)
 app.use('/archivos', uploadRouter)
 app.use('/reporteVentas', repoteVentasRouter)
+app.use('/abonos', paymentRoutes)
 app.use('/facturas', FacturaRoute)
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT ?? 1234}`))
