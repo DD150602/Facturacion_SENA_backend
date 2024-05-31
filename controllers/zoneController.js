524aa88951f60f48e0926a2bcf19d645ad0374c5
DMAGUILARg <111946899+DMAGUILARg@users.noreply.github.com>
2024-05-31 06:07:22 -0500

    fix a la rama crudZone

---
+++ controllers/zoneController.js
@@ -0,0 +1,91 @@
+import ZoneModel from '../models/zoneModel.js';
+import { validateZona, validateZonaUpdate } from '../schemas/zone.js';
+import { NoData } from '../schemas/errorSchema.js';
+
+export default class zoneController{
+  static async getAllZones  (req, res)  {
+    const response = await ZoneModel.getAllzone()
+    if(response instanceof NoData){
+      res.status(404).json({ message: 'No hay zonas registradas' })
+    } else if (response instanceof Error){
+      res.status(500).json({ message: 'Error en el servidor' })
+    } else{
+      res.json(response)
+    }
+  };
+  
+  static async createZone (req, res) {
+     const data = validateZona(req.body)
+     if(!data.success){
+        return res.status(400).json({ message: JSON.parse(data.error.message)[0].message })
+     }
+     const datos = await ZoneModel.createZone(data.data)
+     if(datos instanceof NoData){
+        res.status(404)({ message : 'No se insertaron los datos' })
+     } else if ( datos instanceof Error) {
+        res.status(500)({ message : 'Error interno en el servidor'})
+     } else {
+        res.status(200).json({ message: 'Zona registrada con exito' })
+     }
+  };
+  
+  static async updatedZone (req, res){
+    const { id } = req.params
+    const updata = validateZonaUpdate(req.body)
+  
+
+    if(!updata.success){
+      return res.status(400).json({ message: JSON.parse(updata.error.message)[0].message })
+    }
+    const response = await ZoneModel.updateZone( id, updata.data )
+    if(response instanceof NoData){
+      res.status(404)({ message : 'Esta zona ya existe' })
+   } else if (response instanceof Error) {
+      res.status(500)({ message : 'Error interno en el servidor'})
+   } else {
+      res.json({ message: 'Zona actualizada con exito' })
+   }
+  };
+  
+  
+  static async addUserToZone(req, res){
+    const { id } = req.params
+    const zonaId = req.body.id[0]
+    const response = await ZoneModel.addUserZone(id, zonaId)
+    if (response instanceof NoData) {
+      res.status(400).json({ message: 'No se encontro al usuario para vincular' })
+    } else if (response instanceof Error) {
+      res.status(500).json({ message: 'Error interno del servidor ' })
+    } else {
+      res.json({ message: 'Vinculacion exitosa' })
+    }
+  };
+
+  static async getUserById(req, res){
+    const { id } = req.params
+    const response = await ZoneModel.getUser(id)
+    if(response instanceof NoData){
+        res.status(404).json({ message: 'El usuario no eviste' })
+      } else if (response instanceof Error){
+        res.status(500).json({ message: 'Error en el servidor' })
+      } else{
+        res.json(response)
+      }
+  }
+
+  static async getZonaById(req, res){
+    const { id } = req.params
+    const response = await ZoneModel.getZoneByID(id)
+    if(response instanceof NoData){
+      res.status(404).json({ message: 'No hay zonas registradas' })
+    } else if (response instanceof Error){
+      res.status(500).json({ message: 'Error en el servidor' })
+    } else{
+      res.json(response)
+    }
+  }
+  
+}
+
+
+
---
+++ models/zoneModel.js
@@ -0,0 +1,101 @@
+import db from '../config/database.js'
+import { NoData, DuplicateInfo  } from '../schemas/errorSchema.js'
+
+export default class zoneModel {
+  static async getAllzone () {
+    try {
+      const [res] = await db.query(
+        `
+        SELECT BIN_TO_UUID(id_zona) id, nombre_zona, descripcion_zona 
+        FROM zonas
+        `
+      )
+      if (!res) return new NoData()
+      if (res.length === 0) return new NoData()
+      return res
+    } catch (error) {
+      return error
+    }
+  }
+  static async getZoneByID (id) {
+    try {
+      const [res] = await db.query(
+        `
+        SELECT BIN_TO_UUID(id_zona) id, nombre_zona, descripcion_zona 
+        FROM zonas WHERE id_zona = UUID_TO_BIN(?)
+        `, [id]
+      )
+      if (!res) return new NoData()
+      if (res.length === 0) return new NoData()
+      return res
+    } catch (error) {
+      return error
+    }
+  }
+
+  static async createZone(input){
+      const  { nombreZona , descripcionZona } = input
+    try{
+      const insert = await db.query('INSERT INTO zonas (nombre_zona, descripcion_zona) VALUES (?,?)', [nombreZona, descripcionZona])
+      if(!insert) return new NoData()
+      if(insert.length === 0) return new NoData()
+      return insert
+    }catch (error){
+      return error 
+    }
+  }
+
+  static async updateZone(id, input){
+    try{
+      const { nombreZona, descripcionZona } = input
+
+      const [verificar] = await db.query('SELECT * FROM zonas WHERE nombre_zona = ?',[nombreZona])
+      if (verificar.length > 0) throw new DuplicateInfo()
+
+      const [update] = await db.query('UPDATE zonas SET nombre_zona = ? , descripcion_zona = ? WHERE id_zona = UUID_TO_BIN(?)',[nombreZona,descripcionZona, id])
+
+      return update
+    }catch(error){
+      return error
+    }
+  }
+
+  static async getUser(id) {
+    try {
+        const [responseUser] = await db.query(
+            `
+            SELECT BIN_TO_UUID(id_usuario) AS id, correo_usuario,
+            numero_documento_usuario, 
+            primer_nombre_usuario, 
+            primer_apellido_usuario,
+            telefono_usuario,
+            direccion_usuario
+            FROM usuarios
+            WHERE numero_documento_usuario = ? AND estado_usuario = 1
+            `,
+            [id]
+        );
+        if (!responseUser || responseUser.length === 0) return new NoData();
+        return responseUser;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+}
+
+static async addUserZone(id, zonaId){
+  try{
+      const response = await db.query(`UPDATE usuarios SET id_zona = UUID_TO_BIN(?) WHERE id_usuario = ?`, [zonaId, id]);
+      if(!response) return new NoData()
+        if(response.length === 0) return new NoData()
+        return response
+  } catch (error) {
+      console.log(error);
+      return error;
+  }
+}
+
+
+
+
+}
