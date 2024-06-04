144a44b212d9acc8f1a1c9756b14f1293b003557
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-06-03 23:52:35 -0500

    ✨ traer info de contacto de usuarios

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -5,4 +5,5 @@ export const ClientRoute = Router()
 
 ClientRoute.get('/:numeroDocumentoClient', ClientController.verifyClient)
 ClientRoute.get('/todos/clientes', ClientController.getData)
+ClientRoute.get('/contacto/:id', ClientController.getContactInfoCliente)
 ClientRoute.post('/', ClientController.createClient)
