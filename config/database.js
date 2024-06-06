74b66675f032188daf33d32c2bae919f5dde97d3
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-06-05 20:29:59 -0500

    🗃️ acceso a la base de datos remota

--- config/database.js
+++ config/database.js
@@ -1,12 +1,19 @@
 import mysql2 from 'mysql2/promise'
 import { DATABASE_NAME, DATABASE_PORT, DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USER } from './envVariables.js'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+import path from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const uploadPath = path.join(__dirname, '../certificados/DigiCertGlobalRootCA.crt.pem')
 
 const CONNECTION_STRING = {
   host: DATABASE_HOST,
   user: DATABASE_USER,
   port: DATABASE_PORT,
   password: DATABASE_PASSWORD,
-  database: DATABASE_NAME
+  database: DATABASE_NAME,
+  ssl: { ca: fs.readFileSync(uploadPath) }
 }
 
 let db
