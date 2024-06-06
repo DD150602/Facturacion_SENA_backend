8444bdeb7b4abbd62e5d12371f30da9a10101bdd
Dylan Delgado <121521754+DD150602@users.noreply.github.com>
2024-06-05 19:49:21 -0500

    Update uploadFilesModel.js
    correccion de error

--- models/uploadFilesModel.js
+++ models/uploadFilesModel.js
@@ -4,15 +4,15 @@ import { fileURLToPath } from 'url'
 import path from 'path'
 import { getDownloadURL, uploadBytes, ref } from 'firebase/storage' // Importar las funciones necesarias de Firebase Storage
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export class UploadFilesModel {
   static async uploadFiles (files, dir) {
     try {
       const { archivo } = files
       const timestamp = Date.now()
-      const uploadPath = path.join(dirname, '../temp', `${timestamp}_${archivo.name}`)
+      const uploadPath = path.join(__dirname, '../temp', `${timestamp}_${archivo.name}`)
       await archivo.mv(uploadPath)
       const storageRef = ref(storage, `${dir}/${timestamp}_${archivo.name}`)
       await uploadBytes(storageRef, fs.readFileSync(uploadPath))
@@ -21,7 +21,7 @@ export class UploadFilesModel {
       return downloadURL
     } catch (error) {
       const { archivo } = files
-      const uploadPath = path.join(_dirname, '../temp', `${Date.now()}${archivo.name}`)
+      const uploadPath = path.join(__dirname, '../temp', `${Date.now()}${archivo.name}`)
       if (fs.existsSync(uploadPath)) {
         fs.unlinkSync(uploadPath)
       }
